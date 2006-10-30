327583358a612a5d3d1addb0f9aa4a96409fe1c9
Soren Sandmann <sandmann@daimi.au.dk>
2006-10-30 01:06:46 +0000

    =-=-=-= Release sysprof 1.0.4 =-=-=-=
    2006-10-29  Soren Sandmann <sandmann@daimi.au.dk>
    
            * =-=-=-= Release sysprof 1.0.4 =-=-=-=
    
            * module/sysprof-module.c: Remove include of linux/config.h

--- module/sysprof-module.c
+++ module/sysprof-module.c
@@ -19,7 +19,6 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
-#include <linux/config.h>
 #if !CONFIG_PROFILING
 # error Sysprof needs a kernel with profiling support compiled in.
 #endif

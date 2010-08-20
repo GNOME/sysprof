8237d8c4ca38f91b257212cdcbbefdace8745bda
Jeffrey Stedfast <fejj@gnome.org>
2010-08-20 15:57:41 -0400

    Fixes to build on newer distros

--- autogen.sh
+++ autogen.sh
@@ -20,9 +20,9 @@ DIE=0
 	DIE=1
 }
 
-if automake-1.7 --version < /dev/null > /dev/null 2>&1 ; then
-    AUTOMAKE=automake-1.7
-    ACLOCAL=aclocal-1.7
+if automake-1.11 --version < /dev/null > /dev/null 2>&1 ; then
+    AUTOMAKE=automake-1.11
+    ACLOCAL=aclocal-1.11
 else
 	echo
 	echo "You must have automake 1.7.x installed to compile $PROJECT."

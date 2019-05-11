4670df740e9946ba41bbdbf9306b5bcd0b7fa02f
Christian Hergert <chergert@redhat.com>
2019-05-10 19:32:32 -0700

    libsysprof: add autoclean backports

--- src/libsysprof/sysprof-backport-autocleanups.h
+++ src/libsysprof/sysprof-backport-autocleanups.h
@@ -2,6 +2,8 @@
 
 #include "config.h"
 
+#include <glib.h>
+
 #ifndef HAVE_POLKIT_AUTOPTR
 # include <polkit/polkit.h>
 
@@ -10,3 +12,14 @@
   G_DEFINE_AUTOPTR_CLEANUP_FUNC (PolkitSubject, g_object_unref)
 #endif
 
+#if !GLIB_CHECK_VERSION(2, 56, 0)
+  static inline void
+  g_clear_handle_id (guint *ptr,
+                     void (*clear_func) (guint handle_id))
+  {
+    guint id = *ptr;
+    *ptr = 0;
+    if (id)
+      clear_func (handle_id);
+  }
+#endif

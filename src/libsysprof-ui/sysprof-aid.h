25ad833aaa997c0d926275b34c9ec6172fe9752c
Christian Hergert <chergert@redhat.com>
2019-05-18 14:50:39 -0700

    libsysprof-ui: stub out SysprofAid
    This will be used as a high-level object that knows how to deal with
    both the SysprofSource (data collector) and various views for the user.

---
+++ src/libsysprof-ui/sysprof-aid.h
@@ -0,0 +1,50 @@
+/* sysprof-aid.h
+ *
+ * Copyright 2019 Christian Hergert <chergert@redhat.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#pragma once
+
+#if !defined (SYSPROF_UI_INSIDE) && !defined (SYSPROF_UI_COMPILATION)
+# error "Only <sysprof-ui.h> can be included directly."
+#endif
+
+#include <gio/gio.h>
+#include <sysprof.h>
+
+G_BEGIN_DECLS
+
+#define SYSPROF_TYPE_AID (sysprof_aid_get_type())
+
+SYSPROF_AVAILABLE_IN_ALL
+G_DECLARE_DERIVABLE_TYPE (SysprofAid, sysprof_aid, SYSPROF, AID, GObject)
+
+struct _SysprofAidClass
+{
+  GObjectClass parent_class;
+
+  /*< gpointer >*/
+  gpointer _reserved[16];
+};
+
+SYSPROF_AVAILABLE_IN_ALL
+const gchar *sysprof_aid_get_display_name (SysprofAid *self);
+SYSPROF_AVAILABLE_IN_ALL
+GIcon       *sysprof_aid_get_icon         (SysprofAid *self);
+
+G_END_DECLS

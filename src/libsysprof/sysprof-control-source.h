8aa5b92eea37b6f34f3def596892d0104151b2db
Christian Hergert <chergert@redhat.com>
2020-02-08 07:32:47 -0800

    start on control fd source

---
+++ src/libsysprof/sysprof-control-source.h
@@ -0,0 +1,33 @@
+/* sysprof-control-source.h
+ *
+ * Copyright 2020 Christian Hergert <chergert@redhat.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#pragma once
+
+#include "sysprof-source.h"
+
+G_BEGIN_DECLS
+
+#define SYSPROF_TYPE_CONTROL_SOURCE (sysprof_control_source_get_type())
+
+G_DECLARE_FINAL_TYPE (SysprofControlSource, sysprof_control_source, SYSPROF, CONTROL_SOURCE, GObject)
+
+SysprofControlSource *sysprof_control_source_new (void);
+
+G_END_DECLS

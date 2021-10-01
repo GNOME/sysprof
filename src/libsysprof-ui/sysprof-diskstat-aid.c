556799bc2a400c6ddc778527873cb54e4aaf7fc3
Christian Hergert <chergert@redhat.com>
2021-10-01 10:21:05 -0700

    diskstataid: port to GTK 4

--- src/libsysprof-ui/sysprof-diskstat-aid.c
+++ src/libsysprof-ui/sysprof-diskstat-aid.c
@@ -245,7 +245,7 @@ sysprof_diskstat_aid_present_finish (SysprofAid    *aid,
       if (counters->len > 0)
         sysprof_display_add_group (present->display, group);
       else
-        gtk_widget_destroy (GTK_WIDGET (group));
+        g_object_unref (group);
     }
 
   return counters != NULL;

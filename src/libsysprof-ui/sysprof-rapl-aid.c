b81457c0eada25882f244aa385064e2bfbb37924
Christian Hergert <chergert@redhat.com>
2021-10-01 10:50:43 -0700

    raplaid: port to GTK 4

--- src/libsysprof-ui/sysprof-rapl-aid.c
+++ src/libsysprof-ui/sysprof-rapl-aid.c
@@ -227,7 +227,7 @@ sysprof_rapl_aid_present_finish (SysprofAid    *aid,
       if (found > 0)
         sysprof_display_add_group (present->display, energy);
       else
-        gtk_widget_destroy (GTK_WIDGET (energy));
+        g_object_unref (energy);
     }
 
   return counters != NULL;

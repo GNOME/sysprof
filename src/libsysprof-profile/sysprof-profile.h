0288e937e4745e4cfeab438a60772b65571cb90a
Christian Hergert <chergert@redhat.com>
2023-06-02 18:01:18 -0700

    libsysprof-profile: add SysprofMemoryUsage
    Ports sysprof-memory-source.c to the new SysprofInstrument API.

--- src/libsysprof-profile/sysprof-profile.h
+++ src/libsysprof-profile/sysprof-profile.h
@@ -28,6 +28,7 @@ G_BEGIN_DECLS
 # include "sysprof-cpu-usage.h"
 # include "sysprof-disk-usage.h"
 # include "sysprof-instrument.h"
+# include "sysprof-memory-usage.h"
 # include "sysprof-network-usage.h"
 # include "sysprof-profiler.h"
 # include "sysprof-recording.h"

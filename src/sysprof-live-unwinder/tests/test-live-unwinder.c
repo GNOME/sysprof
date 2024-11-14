2e953d462972ecdf192832f8d0a723beba689386
Christian Hergert <chergert@redhat.com>
2024-11-13 17:16:44 -0800

    tests: fix test on s390

--- src/sysprof-live-unwinder/tests/test-live-unwinder.c
+++ src/sysprof-live-unwinder/tests/test-live-unwinder.c
@@ -59,7 +59,11 @@
 #define SYSPROF_ARCH_PREFERRED_REGS DWARF_NEEDED_REGS
 /* TODO: add other architectures, imitating the linux tools/perf tree */
 #else
-# define SYSPROF_ARCH_PREFERRED_REGS PERF_REG_EXTENDED_MASK
+# ifdef PERF_REG_EXTENDED_MASK
+#  define SYSPROF_ARCH_PREFERRED_REGS PERF_REG_EXTENDED_MASK
+# else
+#  define SYSPROF_ARCH_PREFERRED_REGS 0
+# endif
 #endif /* _ASM_{arch}_PERF_REGS_H */
 
 static gboolean sample_stack;

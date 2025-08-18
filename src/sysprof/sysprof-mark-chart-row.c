60c20f5315870e5d4d51f540b10299b923a903d1
Georges Basile Stavracas Neto <feaneron@igalia.com>
2025-08-18 17:47:49 -0300

    sysprof: Add markchartrow.toggle-catalog action
    This will be used to show or hide individual catalogs from the
    other views.

--- src/sysprof/sysprof-mark-chart-row.c
+++ src/sysprof/sysprof-mark-chart-row.c
@@ -71,6 +71,23 @@ filter_by_mark_action (GtkWidget  *widget,
   sysprof_session_filter_by_mark (session, catalog);
 }
 
+static void
+toggle_catalog_action (GtkWidget  *widget,
+                       const char *action_name,
+                       GVariant   *parameter)
+{
+  SysprofMarkChartRow *self = (SysprofMarkChartRow *)widget;
+  SysprofMarksSectionModelItem *item;
+
+  g_assert (SYSPROF_IS_MARK_CHART_ROW (self));
+
+  if (self->item == NULL)
+    return;
+
+  item = sysprof_mark_chart_item_get_model_item (self->item);
+  sysprof_marks_section_model_item_set_visible (item, !sysprof_marks_section_model_item_get_visible (item));
+}
+
 static gboolean
 sysprof_mark_chart_row_activate_layer_item_cb (SysprofMarkChartRow *self,
                                                SysprofChartLayer   *layer,
@@ -226,6 +243,7 @@ sysprof_mark_chart_row_class_init (SysprofMarkChartRowClass *klass)
   gtk_widget_class_bind_template_callback (widget_class, sysprof_mark_chart_row_activate_layer_item_cb);
   gtk_widget_class_bind_template_callback (widget_class, sysprof_mark_chart_row_button_pressed_cb);
 
+  gtk_widget_class_install_action (widget_class, "markchartrow.toggle-catalog", NULL, toggle_catalog_action);
   gtk_widget_class_install_action (widget_class, "markchartrow.filter-by-mark", NULL, filter_by_mark_action);
 
   g_type_ensure (SYSPROF_TYPE_CHART);

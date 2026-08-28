GNOME/sysprof

commit 03a583e0d8938c7fc96cfb5119e8b2a6138295ec

sysprof: Make SysprofTimeFIlterModel report items properly
If it doesn't update its internal slice model when the source model
changes, we end up with the wrong slice model offset and size.

Closes: https://gitlab.gnome.org/GNOME/sysprof/-/issues/156

src/sysprof/sysprof-time-filter-model.c
@@ -30,6 +30,7 @@ struct _SysprofTimeFilterModel
   GtkExpression     *expression;
   GtkSliceListModel *slice;
   SysprofTimeSpan    time_span;
+  GSignalGroup      *signal_group;
   guint              inclusive : 1;
 };
 
@@ -92,6 +93,7 @@ sysprof_time_filter_model_finalize (GObject *object)
 {
   SysprofTimeFilterModel *self = (SysprofTimeFilterModel *)object;
 
+  g_clear_object (&self->signal_group);
   g_clear_object (&self->slice);
   g_clear_pointer (&self->expression, gtk_expression_unref);
 
@@ -201,6 +203,12 @@ sysprof_time_filter_model_class_init (SysprofTimeFilterModelClass *klass)
 static void
 sysprof_time_filter_model_init (SysprofTimeFilterModel *self)
 {
+  self->signal_group = g_signal_group_new (G_TYPE_LIST_MODEL);
+  g_signal_group_connect_swapped (self->signal_group,
+                                  "items-changed",
+                                  G_CALLBACK (sysprof_time_filter_model_update),
+                                  self);
+
   self->expression = gtk_property_expression_new (SYSPROF_TYPE_DOCUMENT_FRAME, NULL, "time");
   self->inclusive = TRUE;
 
@@ -446,6 +454,7 @@ sysprof_time_filter_model_set_model (SysprofTimeFilterModel *self,
   if (model == sysprof_time_filter_model_get_model (self))
     return;
 
+  g_signal_group_set_target (self->signal_group, model);
   gtk_slice_list_model_set_model (self->slice, model);
   sysprof_time_filter_model_update (self);
   g_object_notify_by_pspec (G_OBJECT (self), properties[PROP_MODEL]);

commit ef3b422ead281446c99393eef2ea9b6a3bdf0cd0

sysprof: fix crash introduced in 03a583e0
I have a syscap file here that was triggering this so finding it was
rather easy. We need to defer until after for this one.

src/sysprof/sysprof-time-filter-model.c
@@ -204,10 +204,12 @@ static void
 sysprof_time_filter_model_init (SysprofTimeFilterModel *self)
 {
   self->signal_group = g_signal_group_new (G_TYPE_LIST_MODEL);
-  g_signal_group_connect_swapped (self->signal_group,
-                                  "items-changed",
-                                  G_CALLBACK (sysprof_time_filter_model_update),
-                                  self);
+  g_signal_group_connect_data (self->signal_group,
+                               "items-changed",
+                               G_CALLBACK (sysprof_time_filter_model_update),
+                               self,
+                               NULL,
+                               (G_CONNECT_AFTER | G_CONNECT_SWAPPED));
 
   self->expression = gtk_property_expression_new (SYSPROF_TYPE_DOCUMENT_FRAME, NULL, "time");
   self->inclusive = TRUE;